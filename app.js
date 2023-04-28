11f01868944a3d867020e1e47d139da29c94beb6
Majd Qumseya <qumseya.majd@gmail.com>
2023-04-28 12:51:54 +0300

    Added new chat and chat history features, finished setting up fetch with ui (needs loader)

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-// do NOT deploy this API key or upload onto Github
-const API_KEY = "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";
+// const API_KEY =  "Your-API-Key" https://platform.openai.com/account/api-keys
 
 const submitButton = document.querySelector("#submit");
+const outputElement = document.querySelector('#output');
+const inputElement = document.querySelector('input');
+const historyElement = document.querySelector('.history');
+const newChatButton = document.querySelector('button');
 
 submitButton.addEventListener("click", getMessage);
+newChatButton.addEventListener("click", resetInput )
+
+function resetInput(){
+    inputElement.value = ''
+    outputElement.textContent = ''
+}
+
+function changeInput(value) {
+    const inputElement = document.querySelector('input');
+    inputElement.value = value
+}
 
 async function getMessage() {
   const options = {
@@ -14,7 +28,7 @@ async function getMessage() {
     },
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "Hello!" }],
+      messages: [{ role: "user", content: inputElement.value }],
       max_tokens: 100
     })
   };
@@ -23,8 +37,22 @@ async function getMessage() {
     const response = await fetch("https://api.openai.com/v1/chat/completions", options);
     const data = await response.json()
     console.log(data)
+   
+    outputElement.textContent = data.choices[0].message.content
+    if(data.choices[0].message.content && inputElement.value){
+        const pElement = document.createElement('p');
+        
+        pElement.textContent = inputElement.value;
+        pElement.addEventListener('click', ()=> changeInput(pElement.textContent ))
+        historyElement.append(pElement)
+
+    }
   } catch (error) {
     console.log(error);
   }
 
 }
+
+
+
+
